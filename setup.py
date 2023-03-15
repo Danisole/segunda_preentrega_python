8f7a9557437d33919acfd61a3eafb3156e3b0e24
Danisole <danisole82@gmail.com>
2023-03-15 00:30:25 -0300

    packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+
+    name= "segunda-preentrega",
+    version= "1.0",
+    description="Paquete preentrega 2 Daniela Reta",
+    author="La Dani",
+    packages=["assets"]
+)
